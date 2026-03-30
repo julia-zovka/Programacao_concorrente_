0160a4f357c201f627343318223592cf5c5267e0
Júlia Zovka de Souza <jzs@cin.ufpe.br>
2026-03-30 11:34:08 -0300

    Create barbeiro.c

---
+++ barbeiro.c
@@ -0,0 +1,13 @@
+/* Barbearia com duas portas (entrada e
+saída) e algumas cadeiras (espera)
+ Barbeiro atende um cliente de cada vez
+ Se faltam clientes, o barbeiro dorme
+ Se um cliente chega, ele acorda o
+barbeiro para que corte seu cabelo
+ Cliente dorme nas cadeiras de espera se
+o barbeiro está ocupado
+*/
+
+get_haircut
+get_next_custumer
+ finished_cut
